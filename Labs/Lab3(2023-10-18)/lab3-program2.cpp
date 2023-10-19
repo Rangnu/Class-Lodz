b217f1770401e9d3c12edc7b0b73fbeb456c5c86
UN RA <147945044+Rangnu@users.noreply.github.com>
2023-10-19 19:46:54 +0200

    Numbers from 20 to 2
    for (int i=20; i>=2; --i)

---
+++ Labs/Lab3(2023-10-18)/lab3-program2.cpp
@@ -0,0 +1,16 @@
+//Program 2
+//2023-19-10
+//write the program that outputs the following numbers 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Numbers from 20 to 2"<<endl;
+     for (int i=20; i>=2; --i)
+     {
+        cout<<i<<" ";
+     }
+}
+
