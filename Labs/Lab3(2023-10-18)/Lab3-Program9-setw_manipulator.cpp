ca6829bad0a95640ccbcd3dcd9c321b91a67bf9f
UN RA <147945044+Rangnu@users.noreply.github.com>
2023-10-21 15:36:29 +0200

    setw manipulator

---
+++ Labs/Lab3(2023-10-18)/Lab3-Program9-setw_manipulator.cpp
@@ -0,0 +1,14 @@
+//setw manipulator
+
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    cout<<setw(10)<<"English"<<setw(5)<<9<<endl;
+    cout<<setw(10)<<"Computer"<<setw(5)<<98<<endl;
+    cout<<setw(10)<<"Math"<<setw(5)<<70<<endl;
+    cout<<setw(10)<<"Physics"<<setw(5)<<5<<endl;
+    return 0;
+}
